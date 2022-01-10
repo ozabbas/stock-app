1eedafbbac81a39bf237073be4c35aa931ca6b82
Owais Abbas <ozabbas95@gmail.com>
2022-01-10 04:40:05 -0600

    added flash messages for search stocks form

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,8 +1,13 @@
 class StocksController < ApplicationController
 
     def search
-       stock = Stock.new_lookup(params[:stock]) 
-       render json: stock
+        if params[:stock].present?
+            @stock = Stock.new_lookup(params[:stock]) 
+            render 'users/my_portfolio'
+        else
+            flash[:alert] = "Please enter a symbol"
+            redirect_to my_portfolio_path
+        end
     end
 
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -6,7 +6,7 @@ class Stock < ApplicationRecord
             secret_token: Rails.application.credentials.iex_client[:sandbox_secret_key],
             endpoint: 'https://sandbox.iexapis.com/v1'
         )
-        client.price(ticker_symbol) #last statement automatically returned
+        new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
     end
 
 end
