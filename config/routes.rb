59dd5b2fc41071383b88564fecf69b08669869f6
Owais Abbas <ozabbas95@gmail.com>
2022-01-10 04:06:57 -0600

    display stock price in browser

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   #get 'users/my_portfolio'
   get 'my_portfolio', to: 'users#my_portfolio'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'search_stock', to: 'stocks#search'
 end
