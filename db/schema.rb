41f9314aa8dc2381e54b8cce3729a0d7717986a6
Owais Abbas <ozabbas95@gmail.com>
2022-01-10 01:10:53 -0600

    added flash messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_29_110306) do
+ActiveRecord::Schema.define(version: 2022_01_08_235304) do
+
+  create_table "friendships", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
